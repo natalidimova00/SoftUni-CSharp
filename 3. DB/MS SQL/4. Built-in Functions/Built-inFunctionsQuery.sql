604939eb33591cde58e3ef8d98d18cef5bbe7abf
natalidimova00 <natalidimova00@abv.bg>
2025-04-01 13:47:54 +0300

    Added 4.Built-in Functions query

--- 3. DB/MS SQL/4. Built-in Functions/Built-inFunctionsQuery.sql
+++ 3. DB/MS SQL/4. Built-in Functions/Built-inFunctionsQuery.sql
@@ -38,3 +38,104 @@ WHERE [Name] NOT LIKE '[RBD]%'
 ORDER BY [Name]
 
 -- 08. Create View Employees Hired After 2000 Year
+CREATE VIEW V_EmployeesHiredAfter2000 AS
+SELECT FirstName, LastName
+FROM Employees
+WHERE DATEPART(YEAR, HireDate) > 2000
+
+-- 09. Length of Last Name
+SELECT FirstName, LastName
+FROM Employees
+WHERE LEN(LastName) = 5
+
+-- 10. Rank Employees by Salary
+SELECT EmployeeID, FirstName, LastName, Salary,
+	DENSE_RANK() OVER
+	(PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+FROM Employees
+WHERE Salary BETWEEN 10000 AND 50000
+ORDER BY Salary DESC
+
+-- 11. Find All Employees with Rank 2 *
+WITH CTE_RankedEmployees AS
+(
+	SELECT EmployeeID, FirstName, LastName, Salary,
+		DENSE_RANK() OVER
+		(PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+	FROM Employees
+	WHERE Salary BETWEEN 10000 AND 50000
+)
+
+SELECT *
+FROM CTE_RankedEmployees
+WHERE [Rank] = 2
+ORDER BY Salary DESC
+
+-- 12. Countries Holding 'A' 3 or More Times
+USE Geography
+GO
+
+SELECT CountryName, IsoCode
+FROM Countries
+WHERE CountryName LIKE '%A%A%A%'
+ORDER BY IsoCode
+
+-- 13. Mix of Peak and River Names
+SELECT PeakName, RiverName,
+LOWER(CONCAT(SUBSTRING(PeakName, 1, LEN(PeakName) - 1), RiverName)) AS Mix
+FROM Peaks, Rivers
+WHERE RIGHT(PeakName, 1) = LEFT(RiverName, 1)
+ORDER BY Mix
+
+-- 14. Games From 2011 and 2012 Year
+USE Diablo
+
+SELECT TOP(50) [Name], FORMAT([Start], 'yyyy-MM-dd') AS [Start]
+FROM Games
+WHERE DATEPART(YEAR, [Start]) BETWEEN 2011 AND 2012
+ORDER BY [Start], [Name]
+
+-- 15. User Email Providers
+SELECT Username, SUBSTRING(Email, CHARINDEX('@', Email) + 1, LEN(Email)) AS EmailProvider  
+FROM Users
+ORDER BY EmailProvider, Username
+
+-- 16. Get Users with IP Address Like Pattern
+SELECT Username, IpAddress
+FROM Users
+WHERE IpAddress LIKE '___.1_%._%.___'
+ORDER BY Username
+
+-- 17. Show All Games with Duration & Part of the Day
+SELECT [Name], [Part of the Day] =
+	CASE
+		WHEN DATEPART(HOUR, [Start]) < 12 THEN 'Morning'
+		WHEN DATEPART(HOUR, [Start]) < 18 THEN 'Afternoon'
+		ELSE 'Evening'
+	END,
+	[Duration] =
+	CASE
+		WHEN [Duration] <= 3 THEN 'Extra Short'
+		WHEN [Duration] <= 6 THEN 'Short'
+		WHEN [Duration] > 6 THEN 'Long'
+		ELSE 'Extra Long'
+	END
+FROM Games
+ORDER BY [Name], [Duration], [Part of the Day]
+
+-- 18. Orders Table
+CREATE TABLE Orders
+(
+	Id INT PRIMARY KEY IDENTITY,
+	ProductName VARCHAR(60),
+	OrderDate DATETIME2
+)
+
+INSERT INTO Orders VALUES ('Butter', GETDATE()),
+						  ('Milk', GETDATE()),
+ 						  ('Honey', GETDATE())
+
+SELECT ProductName, OrderDate,
+	DATEADD(DAY, 3, OrderDate) AS [Pay Due],
+	DATEADD(MONTH, 1, OrderDate) AS [Delivery Due]
+FROM Orders
